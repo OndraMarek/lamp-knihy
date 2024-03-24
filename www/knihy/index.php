c6ecead7f64be4c0ebf2821bb25af79c90983d00
OndraMarek <czondrejmarek@gmail.com>
2024-03-24 20:26:36 +0100

    layout

--- www/knihy/index.php
+++ www/knihy/index.php
@@ -46,17 +46,17 @@ $result = $conn->query($sql);
         <nav>
         </nav>
         
-        <div class="text-center mt-4 p-5 bg-primary text-white rounded">
+        <div class="text-center mt-4 p-5 bg-primary text-white">
             <h1>Katalog knih</h1>
         </div>
     </header>
     <main>
-        <div class="container">
+        <div class="list-container">
             <?php
             if ($result->num_rows > 0) {
-                echo "<ul>";
+                echo "<ul class='list-group'>";
                 while($row = $result->fetch_assoc()) {
-                    echo "<li class='row'>";
+                    echo "<li class='list-group-item'>";
                     $nazev = $row["kniha_nazev"];
                     $isbn = $row["kniha_isbn"];
                     $rok = $row["kniha_rok"];
@@ -65,20 +65,24 @@ $result = $conn->query($sql);
                     $popis = $row["kniha_popis"];
                     $autori = $row["autori"];
                     $zanry = $row["zanry"];
-                    
-                    echo "<div class='col-md-12'>";
-                    echo "<h2>Název: $nazev</h2>";
-                    echo "ISBN: $isbn";
-                    echo "Rok vydání: $rok";
+                    echo "<div class='row'>";
+                    echo "<div class='col-md-8'>";
+                    echo "<h4>$nazev</h4>";
+                    echo "<span class='isbn'>$isbn</span>";
+                    echo "|";
+                    echo "<span class='genre'>$zanry</span>";
+                    echo "<p class='mb-0'>Autor/ři: <b>$autori</b></p>";
+                    echo "<p class='mb-0'>Vydavatel: <b>$vydavatel</b></p>";
                     echo "</div>";
-                    echo "<div class='col-md-12'>";         
-                    echo "<p>Autoři: $autori</p>";
-                    echo "<p>Vydavatel: $vydavatel</p>";
+                    echo "<div class='col-md-4'>";
+                    echo "<button class='mt-5 btn btn-primary'>Detail</button>";
                     echo "</div>";
-                    echo "<div class='col-md-12'>";
-                    echo "<p>Počet stránek: $pocet</p>";
-                    echo "<p>Žánry: $zanry</p>";
-                    echo "<p>Popis: $popis</p>";
+                    echo "<div class='col-md-8'>";         
+                    echo "<span class='year'>Rok vydání: $rok</span>";
+                    echo "|";
+                    echo "<span class='pages'>Počet stránek: $pocet</span>";
+                    echo "<p class='mb-0'>Popis: $popis</p>";
+                    echo "</div>";
                     echo "</div>";
                     echo "</li>";
                 }
@@ -92,7 +96,7 @@ $result = $conn->query($sql);
         </div>
     </main>
 
-    <footer>
+    <footer class="text-center mt-4 p-5 bg-primary text-white">
         <p>Školní projekt v rámci předmětu Databázové systémy II | © Ondřej Marek</p>
     </footer>
 </body>
